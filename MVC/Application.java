5e753e49a86e4e8fe220b32b959155bd3da6550c
xenogamers <silence@xenogamers.org>
2015-11-17 16:56:25 -0500

    MVC Initial Commit
    Please do not edit it. I will be merging all content shortly.

---
+++ MVC/Application.java
@@ -0,0 +1,34 @@
+import java.awt.BorderLayout;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class Application
+{
+   public JFrame mainFrame;
+
+   public Application()
+   {
+      FrameModel frameModel = new FrameModel();
+
+      MainController controller = new MainController();
+      MainView mainView = new MainView(controller);
+
+      controller.addModel(frameModel);
+      controller.addView(mainView);
+
+      frameModel.defaultSettings();
+
+      mainFrame = new JFrame("Main View");
+      mainFrame.getContentPane().add(mainView, BorderLayout.CENTER);
+      mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+      mainFrame.pack();
+
+      mainFrame.setVisible(true);
+   }
+
+   public static void main(String [] args)
+   {
+      new Application();
+   }
+}
