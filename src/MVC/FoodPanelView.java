2b46ff9c479bc0cea6a4b69325a1294bd845afc3
Jamesk2895 <jamesk995@yahoo.com>
2015-11-19 17:53:19 -0500

    GUI FoodPanel Tuning
    Added Labels and textfields for fat, carbs, and protien

--- src/MVC/FoodPanelView.java
+++ src/MVC/FoodPanelView.java
@@ -7,9 +7,15 @@ public class FoodPanelView extends Panel
 {
    Panel addFood = new Panel();
    
-   Panel calPane = new Panel();
+   Panel statPane = new Panel();
    Label calLabel = new Label("Calories: ");
    TextField calField = new TextField(20);
+   Label fatLabel = new Label("Fat: ");
+   TextField fatField = new TextField(20);
+   Label proLabel = new Label("Protien: ");
+   TextField proField = new TextField(20);
+   Label carbLabel = new Label("Carbs: ");
+   TextField carbField = new TextField(20);
    
    Label addFoodLabel = new Label("Add Food");
    
@@ -21,17 +27,29 @@ public class FoodPanelView extends Panel
 
 	public FoodPanelView(FoodListView fl)
    {
+      
       this.setSize(320, 320);
       this.setBackground(new Color(100,100,100));
       this.setLayout(new GridLayout(1,2));
-      calPane.add(calLabel);
-      calPane.add(calField);
+      calLabel.setFont(addFont);
+      fatLabel.setFont(addFont);
+      proLabel.setFont(addFont);
+      carbLabel.setFont(addFont);
+      statPane.setLayout(new GridLayout(0,2));
+      statPane.add(calLabel);
+      statPane.add(calField);
+      statPane.add(fatLabel);
+      statPane.add(fatField);
+      statPane.add(proLabel);
+      statPane.add(proField);
+      statPane.add(carbLabel);
+      statPane.add(carbField);
       addFood.setLayout(new GridLayout(4,1));
       addFood.add(addFoodLabel);
       addFoodLabel.setFont(addFont);
       searchboxpane.add(foodBox);
       addFood.add(searchboxpane);
-      addFood.add(calPane);
+      addFood.add(statPane);
       addingFood.setLayout(new FlowLayout());
       addingFood.add(addFoodButton);
       addFoodButton.addActionListener(new AddButtonListener(fl, this));
